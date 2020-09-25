37d4b5871c12a526fc316518d8c3734a1c34af78
PonceDiego <daiko_011@hotmail.com>
2020-09-24 23:19:08 -0300

    Moved Msg method from controller to manager

--- src/main/java/com/mindia/avisosnick/controllers/CloudMessagingController.java
+++ src/main/java/com/mindia/avisosnick/controllers/CloudMessagingController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,61 +18,42 @@ import com.google.firebase.messaging.AndroidNotification;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.mindia.avisosnick.managers.CloudMessagingManager;
 
 @RestController
 public class CloudMessagingController {
+	@Autowired
+	CloudMessagingManager manager;
 
 	@PostMapping("/newMessage")
 	public void sendToToken(
-			//@RequestParam String token
-			) {
-		// Send a message to the device corresponding to the provided
-		// registration token.
-		String response;
-		//Token Sony
-//		String token= "dOZv-2maRrW0sFj_c0JdLu:APA91bE_wbWHlZ6yjLlwzyhWiGXwgyNWjJCGnzaPOL56S3iWY1K3yLyL93MGhYtctCCdt4yMT-s2C2UKiTf57sRNpE3_-UMKzdiVZ7MrFaBR-1wCQoibE2HSn3jVYH_v8JOlw8PH2xU9";
-	//Token Fonzo
-		String token="cyPz04QpSqK96YtIn785cE:APA91bFegLKBzmktuQ3UqHSGLV129A5prEDxW27FD4YJllK9AsAABRMPbaKKz6kvtllQWulRKB2ZkRRuyUQc8AufAkeDW5N94gGpbiEkN7Hdn_m9O-A3ikkbP2-7iLzcaOz_uXEIWN2N";
-		
-		try {
-			response = FirebaseMessaging.getInstance().send(androidMessage(token));
-			// Response is a message ID string.
-			System.out.println("Successfully sent message: " + response);
-		} catch (FirebaseMessagingException e) {
-			e.printStackTrace();
-		}
+	// @RequestParam String token
+	) {
+		// Token Sony
+		String token= "dOZv-2maRrW0sFj_c0JdLu:APA91bE_wbWHlZ6yjLlwzyhWiGXwgyNWjJCGnzaPOL56S3iWY1K3yLyL93MGhYtctCCdt4yMT-s2C2UKiTf57sRNpE3_-UMKzdiVZ7MrFaBR-1wCQoibE2HSn3jVYH_v8JOlw8PH2xU9";
+		// Token Fonzo
+//		String token = "cyPz04QpSqK96YtIn785cE:APA91bFegLKBzmktuQ3UqHSGLV129A5prEDxW27FD4YJllK9AsAABRMPbaKKz6kvtllQWulRKB2ZkRRuyUQc8AufAkeDW5N94gGpbiEkN7Hdn_m9O-A3ikkbP2-7iLzcaOz_uXEIWN2N";
+
+		manager.sendMessage(token);
 	}
 
 	@PostConstruct
 	private void firebaseLogIn() {
 		try {
 			System.out.println(System.getProperty("user.dir"));
-			FileInputStream serviceAccount =
-					  new FileInputStream("src/main/resources/avisosnick-firebase-adminsdk-ln9j6-55140aa5db.json");
-			
-					FirebaseOptions options = FirebaseOptions.builder()
-					  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-					  .setDatabaseUrl("https://avisosnick.firebaseio.com")
-					  .build();
+			FileInputStream serviceAccount = new FileInputStream(
+					"src/main/resources/avisosnick-firebase-adminsdk-ln9j6-55140aa5db.json");
 
-					FirebaseApp.initializeApp(options);
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.setDatabaseUrl("https://avisosnick.firebaseio.com").build();
+
+			FirebaseApp.initializeApp(options);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-
 	}
 
-	private Message androidMessage(String token) {
-		// [START android_message]
-		Message message = Message.builder().setAndroidConfig(AndroidConfig.builder().setTtl(3600 * 1000) // 1 hour in
-																											// milliseconds
-				.setPriority(AndroidConfig.Priority.NORMAL)
-				.setNotification(AndroidNotification.builder().setTitle("Test Message using Eclipse.")
-						.setBody("This is a test message created on AvisosNick by Daiko'.").build())
-				.build()).setToken(token).build();
-		// [END android_message]
-		return message;
-	}
 }
--- src/main/java/com/mindia/avisosnick/managers/CloudMessagingManager.java
+++ src/main/java/com/mindia/avisosnick/managers/CloudMessagingManager.java
@@ -1,6 +1,42 @@
 package com.mindia.avisosnick.managers;
 
+import org.springframework.stereotype.Service;
+
+import com.google.firebase.messaging.AndroidConfig;
+import com.google.firebase.messaging.AndroidNotification;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+
+@Service
 public class CloudMessagingManager {
-	
+
+	public void sendMessage(String token) {
+		// Send a message to the device corresponding to the provided
+		// registration token.
+		String response;
+
+		try {
+			response = FirebaseMessaging.getInstance().send(androidMessage(token));
+			// Response is a message ID string.
+			System.out.println("Successfully sent message: " + response);
+		} catch (FirebaseMessagingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Message androidMessage(String token) {
+		// [START android_message]
+		Message message = Message.builder()
+				//.putData("Sender", "Daiko'")
+				.setAndroidConfig(AndroidConfig.builder().setTtl(3600 * 1000) // 1 hour in
+																											// milliseconds
+				.setPriority(AndroidConfig.Priority.NORMAL)
+				.setNotification(AndroidNotification.builder().setTitle("Test Message using Eclipse.")
+						.setBody("This is a test message created on AvisosNick by Daiko'.").build())
+				.build()).setToken(token).build();
+		// [END android_message]
+		return message;
+	}
 
 }
